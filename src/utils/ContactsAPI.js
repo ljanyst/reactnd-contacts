255bbb1cfc98fb6d9d6e9e9c56478361ede76f7f
Lukasz Janyst <lukasz@jany.st>
2017-12-21 02:19:18 +0100

    Fix the template code style issues and add missing semicolons

--- src/utils/ContactsAPI.js
+++ src/utils/ContactsAPI.js
@@ -1,24 +1,24 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:5001'
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:5001';
 
-let token = localStorage.token
+let token = localStorage.token;
 
 if (!token)
-  token = localStorage.token = Math.random().toString(36).substr(-8)
+  token = localStorage.token = Math.random().toString(36).substr(-8);
 
 const headers = {
   'Accept': 'application/json',
   'Authorization': token
-}
+};
 
 export const getAll = () =>
   fetch(`${api}/contacts`, { headers })
-    .then(res => res.json())
-    .then(data => data.contacts)
+  .then(res => res.json())
+  .then(data => data.contacts);
 
 export const remove = (contact) =>
   fetch(`${api}/contacts/${contact.id}`, { method: 'DELETE', headers })
-    .then(res => res.json())
-    .then(data => data.contact)
+  .then(res => res.json())
+  .then(data => data.contact);
 
 export const create = (body) =>
   fetch(`${api}/contacts`, {
@@ -28,4 +28,4 @@ export const create = (body) =>
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(body)
-  }).then(res => res.json())
+  }).then(res => res.json());
