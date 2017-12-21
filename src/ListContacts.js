d8a21d11acb0562bb02a3ad9aa853fef4891488c
Lukasz Janyst <lukasz@jany.st>
2017-12-21 16:09:00 +0100

    Add a controlled component with the search input

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -8,27 +8,46 @@ class ListContacts extends Component {
     onDeleteContact: PropTypes.func.isRequired
   }
 
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query });
+  }
+
   render() {
     return (
-      <ol className='contact-list'>
-        {this.props.contacts.map((contact) => (
-          <li key={contact.id} className='contact-list-item'>
-            <div className='contact-avatar' style={{
-                   backgroundImage: `url(${contact.avatarURL})`
-                 }}>
-            </div>
-            <div className='contact-details'>
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-            <button
-              onClick={() => this.props.onDeleteContact(contact)}
-              className='contact-remove'>
-              Remove
-            </button>
-          </li>
-        ))}
-      </ol>
+      <div className='list-contacts'>
+        <div className='list-contacts-top'>
+          <input
+            className='search-contacts'
+            type='text'
+            placeholder='Search contacts'
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+            />
+        </div>
+        <ol className='contact-list'>
+          {this.props.contacts.map((contact) => (
+            <li key={contact.id} className='contact-list-item'>
+              <div className='contact-avatar' style={{
+                     backgroundImage: `url(${contact.avatarURL})`
+                   }}>
+              </div>
+              <div className='contact-details'>
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+              </div>
+              <button
+                onClick={() => this.props.onDeleteContact(contact)}
+                className='contact-remove'>
+                Remove
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
     );
   }
 }
