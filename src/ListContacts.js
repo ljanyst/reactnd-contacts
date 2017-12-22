557e4ee1651fe9b9f465c41e3a13031eb7ab038d
Lukasz Janyst <lukasz@jany.st>
2017-12-22 10:51:43 +0100

    Add the add contact button to the ListContacts component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import escapeRegExp from 'escape-string-regexp';
 import sortBy from 'sort-by';
@@ -47,6 +48,9 @@ class ListContacts extends Component {
             value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
             />
+            <Link to='/create' className='add-contact'>
+              Add contact
+            </Link>
         </div>
 
         {showingContacts.length !== contacts.length && (
