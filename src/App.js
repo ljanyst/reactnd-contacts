86926173533e83b23a1fc8f4125f3fc5c9a5880a
Lukasz Janyst <lukasz@jany.st>
2017-12-22 12:10:56 +0100

    Implement contact addition

--- src/App.js
+++ src/App.js
@@ -22,6 +22,14 @@ class App extends Component {
     }));
   }
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([contact])
+      }));
+    });
+  }
+
   render() {
     return (
       <div>
@@ -30,7 +38,14 @@ class App extends Component {
                 onDeleteContact={this.removeContact}
                 contacts={this.state.contacts} />
         )}/>
-        <Route path='/create' component={CreateContact} />
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact);
+              history.push('/');
+              }
+            }/>
+        )}/>
       </div>);
   }
 }
--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,10 +1,31 @@
 
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput';
+import formSerialize from 'form-serialize';
 
 class CreateContact extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const values = formSerialize(e.target, { hash: true });
+    this.props.onCreateContact(values);
+  }
   render() {
     return (
-      <div>Create Contact!</div>
+      <div>
+        <Link className='close-create-contact' to='/'>Close</Link>
+        <form className='create-contact-form' onSubmit={this.handleSubmit}>
+          <ImageInput
+            className='create-contact-avatar-input'
+            name='avatarURL'
+            maxHeight={64} />
+          <div className='create-contact-details'>
+            <input type='text' name='name' placeholder='Name' />
+            <input type='text' name='email' placeholder='Email' />
+            <button>Add Contact</button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
